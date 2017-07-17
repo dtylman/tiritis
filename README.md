8cfa0f9e0340250ef4b8812fe44dfc4d07aaa0a1
danny <dtylman@gmail.com>
2017-07-17 09:54:14 +0300

    adding objects and some colors to the alerts

--- README.md
+++ README.md
@@ -15,3 +15,10 @@ if ((e.type==='ADDED')  && (typeof(e.object.spec.tls)==='undefined')){
   alert('Route wihtout TLS not allowed in "' + e.object.metadata.namespace + ":" + e.object.metadata.name+'"');
 }
 ```
+
+Check for privileged in SCC:
+```javascript
+if (e.object.allowPrivilegedContainer){
+  alert("Security context constraints allows creating privileged containers: " + e.object.metadata.name);
+}
+```
--- models/alert.go
+++ models/alert.go
@@ -3,7 +3,7 @@ package models
 import (
 	"github.com/dtylman/tiritis/models/db"
 
-	"fmt"
+	"encoding/json"
 	"github.com/astaxie/beego"
 	"k8s.io/apimachinery/pkg/util/uuid"
 	"time"
@@ -19,15 +19,22 @@ type Alert struct {
 }
 
 func addAlert(inspection string, typ string, message string, event interface{}) {
+	var eventString string
+	eventData, err := json.MarshalIndent(event, "", " ")
+	if err != nil {
+		eventString = err.Error()
+	} else {
+		eventString = string(eventData)
+	}
 	alert := &Alert{
 		ID:         string(uuid.NewUUID()),
 		Time:       time.Now().String(),
 		Inspection: inspection,
 		Type:       typ,
 		Message:    message,
-		Event:      fmt.Sprint("%v", event),
+		Event:      eventString,
 	}
-	err := db.Instance.Save(alert)
+	err = db.Instance.Save(alert)
 	if err != nil {
 		beego.Error(err)
 	}
